705bc99406dae56bb5a682cba1f74e9df2e4bac8
safakferhatkaya <safakferhatkaya@gmail.com>
2022-08-02 00:41:07 +0300

    tested last version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   def index
     if @current_device
-      @devices = Device.where(is_active: true)
+      @devices = Device.where("last_online > ?", 5.minutes.ago).order(id: :desc)
     end
-
   end
 end
